c6d6dd5b88cc18979aec7b1cc75c483d428cfd2d
Anderson Ves <andersonvesg@gmail.com>
2026-01-04 20:01:06 -0300

    vish2

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app.core.database import engine
-from app.models.base import Base
-from app.api.router import api_router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-app.include_router(api_router)
