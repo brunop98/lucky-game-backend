0ef54f40cce5f64d638590d5f09f48b21add455f
Anderson Ves <andersonvesg@gmail.com>
2026-01-04 23:38:51 -0300

    card model

---
+++ app/schemas/card.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+class CardReward(BaseModel):
+    type: str
+    amount: int | None = None # quantidade de grana
+
+    buff: str | None = None # tipo de buff
+    buff_duration_sec: int | None = None # duração do buff em segundos
+
+
+
+    item_slug: str | None = None # referencia a tabela de itens
+
+class Card(BaseModel):
+    id_slug: str # coin_low, coin_medium, jackpot, boost_xp_small, rare_item, common_item...
+    category: str
+    image_url: str
+    rarity: float
+    draw_weight: float
+    reward: CardReward
